ad24462cf6e00ac257e702c0dfb5f4d93e65094c
lauren inacio <lauren_f23@yahoo.com>
2017-12-14 14:46:21 -0500

    full crud functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :destroy]
+
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
@@ -21,17 +24,26 @@ end
 
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
-
   def find_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
