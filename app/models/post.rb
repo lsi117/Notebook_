c21b42f88e32693c614e8f42f252cced4712c330
lauren inacio <lauren_f23@yahoo.com>
2017-12-16 14:53:04 -0500

    working on structure and style

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   acts_as_votable
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_attached_file :image, styles: { medium: "700x500#", thumb: "300x300>" }
+  has_attached_file :image, styles: { medium: "500x450#", thumb: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
